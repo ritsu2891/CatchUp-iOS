3f290498dca5718c95529dd442ef65a718862132
Ritsuki Kokubo <ritsu2891@gmail.com>
2025-03-03 22:59:25 +0900

    hello-swift-ui終了

--- SwiftUI/hello-swiftui/ChatPrototype/ChatPrototype/ContentView.swift
+++ SwiftUI/hello-swiftui/ChatPrototype/ChatPrototype/ContentView.swift
@@ -1,19 +1,14 @@
-//
-//  ContentView.swift
-//  ChatPrototype
-//
-//  Created by 小久保 律樹 on 2025/03/03.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, rpaka!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+            Text("How are you")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
         }
         .padding()
     }
